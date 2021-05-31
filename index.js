b93628316f14516a6189f00635fa828e4bf56a2c
Kirahhellrose <clr@DESKTOP-MVRDDCF.localdomain>
2021-05-31 08:34:47 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const newHeader = document.createElement('H1');
+document.body.appendChild(newHeader);
+newHeader.setAttribute("id","victory");
+
+newHeader.innerHTML = 'Malcolm is the champion';
